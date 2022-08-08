0799df30dc37932ab02cc299c9bccf7dfa0055bf
federico_festa <federico.sfesta@gmail.com>
2022-08-07 21:00:09 -0300

    paso 3

--- practica-session/controllers/user.controller.js
+++ practica-session/controllers/user.controller.js
@@ -1,7 +1,7 @@
 const { validationResult } = require('express-validator')
 
 const userController = {
-  index: function (req, res) {
+  index: (req, res) => {
     const validations = validationResult(req);
     const user = req.body;
     req.session.user = user;
@@ -12,11 +12,11 @@ const userController = {
     if(req.body.recordar == 'on') {
       res.cookie('color', req.body.color, {maxAge: 60000 });
       const color = req.body.color;
-      res.render('index', {user, color});
+      res.render('index', {user, color });
     };
     if(req.cookies.color){
       const color = req.cookies.color;
-      res.render('index', {user, color});
+      res.render('index', {user, color });
     }else{
       const color = req.body.color;
       res.render('index', { user, color });
